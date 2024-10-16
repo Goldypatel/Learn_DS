e3bb55577ca941640794af0047dd04d851eb8522
goldy patel <goldy39.gp@gmail.com>
2024-10-16 23:45:57 +0530

    op on linkelist

---
+++ insert_LL.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node* next;
+        
+        node (int data){
+            this -> data =data;
+            this -> next = nullptr;
+        }
+};
+
+void insertAtHead()
+
+int main()
+{
+    
+    
+
+    return 0;
+}
